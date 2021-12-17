1d5326456bda73f3c60aff9df042093bac3c0394
Dmitry <ldmitry95@gmail.com>
2021-12-17 20:23:56 +0300

    test

---
+++ Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+
+class ProfileView: UIView {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var birthLabel: UILabel!
+    
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    @IBOutlet weak var avatarImageView: UIImageView!
+    
+    @IBOutlet weak var discriptionLabel: UITextView!
+}
+
+
---
+++ Netology_IB_Instruments/ProfileViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by Dmitry Lopatin on 17.12.2021.
+//
+
+import UIKit
+
+
+class ProfileViewController: UIViewController {
+    
+}
